f48b649262e6a993ac7466029b75ffbff5eefcbb
Selene Choi <selenechoi0214@gmail.com>
2023-12-04 18:47:29 -0800

    LandingScreen, OnboardingScreen
    LandingScreen css finished
    OnboardingScreen basic structure ready

--- mymori/screens/LandingScreen.js
+++ mymori/screens/LandingScreen.js
@@ -1,26 +1,111 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Animated, ActivityIndicator, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Animated, ActivityIndicator, Text, Image, Button } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+
+import { useFonts, Comfortaa_400Regular, Kalam_400Regular } from "@expo-google-fonts/comfortaa";
 
 export default function LandingScreen({ navigation }) {
 
     const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+    const [fontsLoaded] = useFonts({
+        Comfortaa_400Regular
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator />
+                <Text style={styles.loading}>
+                    Loading...
+                </Text>
+            </View>
+        );
+    }
 
     return (
         <AnimatedLinearGradient
             style={styles.container}
             colors={["rgba(75, 22, 127, 1)", "rgba(255, 166, 195, 1)"]}
         >
-            <Text style={styles.font}>LANDING PAGE</Text>
-        </AnimatedLinearGradient>
+            <View style={styles.top}>
+                <Image
+                    style={styles.logo}
+                    source={require('../assets/logo.png')}
+                />
+                <Text style={styles.heading}>MyMori</Text>
+                <Text style={styles.blurb}>bridges the silence</Text>
+            </View>
+
+            <View style={styles.centerContainer}>
+                <Image
+                    style={styles.tree}
+                    source={require('../assets/LandingTree.png')}
+                />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Onboarding')}
+                    >
+                        <Text style={styles.button}>
+                            Start Your Journey
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </AnimatedLinearGradient >
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        position: 'relative',
     },
-    font: {
-        fontFamily: '',
+    top: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 120,
+    },
+    logo: {},
+    heading: {
+        fontSize: 32,
+        fontWeight: '400',
+        color: '#98DE8D',
+        textAlign: 'center',
+        marginTop: 5,
+        fontFamily: 'Comfortaa_400Regular',
+    },
+    blurb: {
+        fontSize: 24,
+        fontWeight: '400',
+        color: '#98DE8D',
+        textAlign: 'center',
+        marginTop: 15,
+        fontFamily: 'Comfortaa_400Regular',
+    },
+    centerContainer: {
+        marginTop: 0,
+    },
+    tree: {
+        width: 420,
+        height: 420,
+    },
+    buttonContainer: {
+        alignSelf: 'center',
+        // width: 210,
+        position: 'absolute',
+        bottom: 60,
+        backgroundColor: "#BBAAFB",
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    button: {
+        fontFamily: 'Comfortaa_400Regular',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 16,
     }
 });
--- mymori/screens/OnboardingScreen.js
+++ mymori/screens/OnboardingScreen.js
@@ -1,23 +1,68 @@
-import { StyleSheet, View, TouchableOpacity, Animated, ActivityIndicator, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Animated, ActivityIndicator, Text, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 
-export default function OnboardingScreen({navigation}){
-    
+export default function OnboardingScreen({ navigation }) {
+
     const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
-    return(
+    return (
         <AnimatedLinearGradient
-        style={styles.container}
-        colors={["rgba(75, 22, 127, 1)", "rgba(255, 166, 195, 1)"]}
+            style={styles.container}
+            colors={["rgba(75, 22, 127, 1)", "rgba(255, 166, 195, 1)"]}
         >
-         <Text>ONBOARDING</Text>   
+            <TouchableOpacity>
+                <Image
+                    style={styles.account}
+                    source={require('../assets/account.png')}
+                />
+            </TouchableOpacity>
+            <View style={styles.centerContainer}>
+                <Image
+                    source={require('../assets/onboarding.png')}
+                />
+            </View>
+            <View style={styles.bottom}>
+                <TouchableOpacity>
+                    <FontAwesomeIcon
+                        name="chevron-circle-left"
+                        size={40}
+                        color="#4B167F"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontAwesomeIcon
+                        name="chevron-circle-right"
+                        size={40}
+                        color="#4B167F"
+                    />
+                </TouchableOpacity>
+
+            </View>
         </AnimatedLinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
-    container : {
+    container: {
         flex: 1,
-        
+    },
+    account: {
+        alignSelf: 'flex-end',
+        resizeMode: 'contain',
+        width: 50,
+        marginTop: 40,
+        marginRight: 20,
+    },
+    centerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    bottom:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginBottom: 40,
     },
 });
